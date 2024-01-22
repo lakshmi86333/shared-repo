eef96df3b7ad45634f1d081470ce8ff3d7d6f888
lakshmi86333 <50592556+lakshmi86333@users.noreply.github.com>
2024-01-22 13:02:19 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,7 @@
+def call(ip,creds,user){
+                sshagent(["${tomcat}"]) {
+                sh "scp target/*.war ${user}@${ip}:/opt/tomcat8/webapps"
+                sh "ssh ${user}@${ip} /opt/tomcat8/bin/shutdown.sh "
+                sh "ssh ${user}@${ip} /opt/tomcat8/bin/startup.sh "
+}
+}
